e2e4015951e7f0d83752db6063dabd59a7cf042c
samuck21 <samu105100p@gmail.com>
2025-07-05 22:02:47 -0600

    Exercise 1

--- src/Arrays/ExcerciceOne/IsUnique.java
+++ src/Arrays/ExcerciceOne/IsUnique.java
@@ -1,4 +1,19 @@
 package Arrays.ExcerciceOne;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class IsUnique {
+    private static int NUMBERS_OF_CHARS =128;
+    public static boolean isUnique(String string){
+        if(string.length() >NUMBERS_OF_CHARS)return false;
+        Set<Character> characters = new HashSet<>();
+        for(Character c: string.toCharArray()){
+            if(characters.contains(c))return false;
+            characters.add(c);
+        }
+
+        return  true;
+    }
+
 }
--- src/Arrays/ExcerciceTwo/TwoSum.java
+++ src/Arrays/ExcerciceTwo/TwoSum.java
@@ -1,4 +1,18 @@
 package Arrays.ExcerciceTwo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+    public int [] twoSum(int[]numbers, int targert){
+        if(numbers==null||numbers.length<2) return null;
+        Map<Integer,Integer> compMap = new HashMap<>();
+        for(int i=0;i<numbers.length;i++){
+            if(compMap.containsKey(numbers[i]))
+                return  new int[]{i,compMap.get(numbers[i])};
+            int complement = targert-numbers[i];
+            compMap.put(complement,i);
+        }
+        return null;
+    }
 }
